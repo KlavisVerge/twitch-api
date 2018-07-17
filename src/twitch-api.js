f8850116aef0d69a7f99a625460713627927a255
Nick Phillips <imprecatorr@gmail.com>
2018-07-16 21:22:06 -0500

    Initial commit of base interaction with twitch-api

---
+++ src/twitch-api.js
@@ -0,0 +1,50 @@
+require('dotenv/config');
+const AWS = require('aws-sdk');
+const request = require('request-promise');
+AWS.config.update({region: 'us-east-1'});
+
+exports.handler = (event, context) => {
+    if(event){
+        if(!event.body){
+            event.body = {};
+        }else if(typeof event.body === 'string'){
+            event.body = JSON.parse(event.body);
+        }
+    }
+    const required = ['gameName'].filter((property) => !event.body[property]);
+    if(required.length > 0){
+        return Promise.reject({
+            statusCode: 400,
+            message: `Required properties missing: "${required.join('", "')}".`
+        });
+    }
+    let promises = [];
+    var options = {
+        url: 'https://api.twitch.tv/helix/games?name=' + event.body.gameName,
+        headers: {
+            'Client-ID': process.env.CLIENT_ID
+        }
+    };
+    promises.push(request(options).promise().then((res) => {
+        return res;
+    }).catch(function (err) {
+        return Promise.reject({
+            statusCode: err.statusCode,
+            message: 'Error interacting with Twitch API.'
+        });
+    }));
+
+    return Promise.all(promises).then((responses) => {
+        const[results] = responses;
+        return context.succeed({
+            statusCode: 200,
+            body: JSON.stringify(results),
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Methods': 'POST',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,XAmz-Security-Token',
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
+    });
+}
---
+++ terraform.tf
@@ -0,0 +1,73 @@
+terraform {
+  backend "local" {
+    path = "tf_backend/twitch-api.tfstate"
+  }
+}
+
+variable "REST_API_ID" {}
+variable "PARENT_ID" {}
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "CLIENT_ID" {}
+
+data "aws_iam_role" "role" {
+  name = "apis-for-all-service-account"
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+}
+
+resource "aws_api_gateway_resource" "twitch-api-resource" {
+  rest_api_id = "${var.REST_API_ID}"
+  parent_id   = "${var.PARENT_ID}"
+  path_part   = "twitch-api"
+}
+
+resource "aws_lambda_function" "twitch-api-function" {
+  filename      = "twitch-api.zip"
+  function_name = "twitch-api"
+
+  role             = "${data.aws_iam_role.role.arn}"
+  handler          = "src/twitch-api.handler"
+  source_code_hash = "${base64sha256(file("twitch-api.zip"))}"
+  runtime          = "nodejs6.10"
+  timeout          = 20
+
+  environment {
+    variables {
+      CLIENT_ID = "${var.CLIENT_ID}"
+    }
+  }
+}
+
+resource "aws_lambda_permission" "twitch-permission" {
+  function_name = "${aws_lambda_function.twitch-api-function.function_name}"
+  statement_id  = "AllowExecutionFromApiGateway"
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_api_gateway_method" "twitch-api-method-post" {
+  rest_api_id   = "${var.REST_API_ID}"
+  resource_id   = "${aws_api_gateway_resource.twitch-api-resource.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "twitch-api-integration" {
+  rest_api_id             = "${var.REST_API_ID}"
+  resource_id             = "${aws_api_gateway_resource.twitch-api-resource.id}"
+  http_method             = "POST"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.twitch-api-function.invoke_arn}"
+}
+
+module "CORS_FUNCTION_DETAILS" {
+  source      = "github.com/carrot/terraform-api-gateway-cors-module"
+  resource_id = "${aws_api_gateway_resource.twitch-api-resource.id}"
+  rest_api_id = "${var.REST_API_ID}"
+}
